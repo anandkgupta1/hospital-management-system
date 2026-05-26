8565e938c0be5dd83c0abc50f99de391020f1f0d
Anand Kumar Gupta <154501035+anandkgupta1@users.noreply.github.com>
2026-05-26 16:32:29 +0530

    Refactor Doctor methods for improved resource handling
    Refactored viewDoctors and getDoctorById methods to use try-with-resources for better resource management and improved output formatting.

--- src/HospitalManagementSystem/Doctor.java
+++ src/HospitalManagementSystem/Doctor.java
@@ -4,52 +4,54 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Doctor {
+
     private Connection connection;
 
-
-    public Doctor(Connection connection){
+    public Doctor(Connection connection) {
         this.connection = connection;
     }
 
-    public void viewDoctors(){
-        String query = "Select * from doctors";
-        try{
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println("Doctors: ");
-            System.out.println("+------------+-----------------+-------------------+");
-            System.out.println("| Doctor Id | Name            | Specialization     |");
-            System.out.println("+------------+-----------------+-------------------+");
-            while(resultSet.next()){
+    public void viewDoctors() {
+        String query = "SELECT * FROM doctors";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            System.out.println("\nDoctors:");
+            System.out.println("+------------+---------------------+---------------------+");
+            System.out.println("| Doctor Id  | Name                | Specialization      |");
+            System.out.println("+------------+---------------------+---------------------+");
+
+            boolean hasResults = false;
+            while (resultSet.next()) {
+                hasResults = true;
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
                 String specialization = resultSet.getString("specialization");
-                System.out.printf(" |%-12s|%-19s|%-19s|\n", id, name, specialization);
-                System.out.println("+------------+-----------------+-------------------+");
+                System.out.printf("| %-10d | %-19s | %-19s |%n", id, name, specialization);
+                System.out.println("+------------+---------------------+---------------------+");
             }
 
-        }catch(SQLException e){
-            e.printStackTrace();
+            if (!hasResults) {
+                System.out.println("| No doctors found.                                      |");
+                System.out.println("+------------+---------------------+---------------------+");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error retrieving doctors: " + e.getMessage());
         }
     }
 
-    public boolean getDoctorById(int id){
-        String query = "SELECT * from doctors where id = ?";
-        try{
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+    public boolean getDoctorById(int id) {
+        String query = "SELECT id FROM doctors WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                return true;
-            }else{
-                return false;
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
             }
-
-        }catch (SQLException e){
-            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error finding doctor: " + e.getMessage());
         }
         return false;
     }
